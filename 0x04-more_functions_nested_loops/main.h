094970e0786be282dafc812c382810c16dc6f4ef
Wenslaus <sifunawenslaus@gmail.com>
2022-09-15 16:32:38 -0700

    My task 5

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ *main - header files for prototypes
+ *void prototypes (void)
+ *int prototypes (int)
+ */
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,3 +15,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+#endif /* MAIN_H */
