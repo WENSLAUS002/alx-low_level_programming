fe5331aee52c871e61204048eb3ae3a45bc9bb7d
Wenslaus <sifunawenslaus@gmail.com>
2022-10-11 09:17:40 -0700

    My task 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,13 @@
-#include <stdio.h>
-#include "dog.h"
-/**
-* main - check the code for Holberton School students.
-*
-* Return: Always 0.
-*/
-int main(void)
-{
-struct dog my_dog;
-my_dog.name = "Django";
-my_dog.age = 3.5;
-my_dog.owner = "Jay";
-printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-return (0);
-}
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void print_number(int n);
+
+#endif /* MAIN_H */
+
+                   
